6ce18c26e163c1799d1ad98490e4f489eb2ad182
chesnokovdk <chesnokovdk@gmail.com>
2024-07-20 20:31:21 +0300

    fist 2 scripts

---
+++ script_2.1.py
@@ -0,0 +1,18 @@
+# -----------------------
+# our_number = 7
+# print("Guess the number in the range from 1 to 10.")
+
+# while 1:
+#     guess = int(input("Guess: "))
+#     if guess < our_number:
+#         print("The number is higher!")
+#     elif guess > our_number:
+#         print("The number is lower!")
+#     elif guess == our_number:
+#         print("Correct!")
+#         break 
+# -----------------------
+
+from random import randint
+
+lower_num, higher
